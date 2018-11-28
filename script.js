6cbf37de43fa244ed45d92f06e103e3215d9454d
poetrywithalyssa <42788201+poetrywithalyssa@users.noreply.github.com>
2018-11-27 19:05:34 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.parallax');
-    var instances = M.Parallax.init(elems);
-  });
-
 $(".chonkiest").on("click", function() {
-	$(".chonkiest").show("https://pm1.narvii.com/6926/e99eaec16986161c6363b2d25f8e1bea85fa0aaar1-1396-785v2_hq.jpg");
-	$(".chonkiest").text("YEP, OHLAWDHECOMIN")
+	$(".chonkiest").remove()
+	$(".fine").remove()
+	$(".hefty").remove()
+	$(".result-1").append("<img class='responsive-img' src='https://pm1.narvii.com/6926/e99eaec16986161c6363b2d25f8e1bea85fa0aaar1-1396-785v2_hq.jpg'>")
+	$(".result-1").append("<h1>YEP, OHLAWDHECOMIN</h1>");
 });
 
 $(".fine").on("click", function() {
-	$(".fine").show("https://news.nationalgeographic.com/content/dam/news/2018/05/17/you-can-train-your-cat/02-cat-training-NationalGeographic_1484324.ngsversion.1526587209178.adapt.1900.1.jpg");
-	$(".fine").text("Nope, they be A fine boi")
+	$(".chonkiest").remove()
+	$(".fine").remove()
+	$(".hefty").remove()
+	$(".result-2").append("<img class='responsive-img' src='https://news.nationalgeographic.com/content/dam/news/2018/05/17/you-can-train-your-cat/02-cat-training-NationalGeographic_1484324.ngsversion.1526587209178.adapt.1900.1.jpg'>")
+	$(".result-2").append("<h1>Nope, they be A fine boi</h1>");
 });
 
 $(".hefty").on("click", function () {
-	$(".hefty").show("https://i1.wp.com/catoverdose.com/wp-content/uploads/2015/11/chartreux-large-cat.jpg?resize=560%2C399&ssl=1");
-	$("hefty").text("Eh, just a HEFTYCHONK")
+	$(".chonkiest").remove()
+	$(".fine").remove()
+	$(".hefty").remove()
+	$(".result-3").append("<img class='responsive-img' src='https://www.fmdos.cl/wp-content/uploads/2018/07/gato-1.jpg'>")
+	$(".result-3").append("<h1>Eh, just a HEFTYCHONK</h1>");
 });
-
